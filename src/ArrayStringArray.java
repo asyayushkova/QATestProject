2aa00d08c7c6910eec12a76d44dc478a24b5bdae
asyayushkova <yushkova.asya@gmail.com>
2022-11-02 21:42:45 +0300

    Create ArrayStringArray.java

---
+++ src/ArrayStringArray.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class ArrayStringArray
+  {
+      //Scanner scanner = new Scanner(System.in);
+      //System.out.println ("ВВедите число");
+      //int n = scanner.nextInt();
+
+      int [] arrayOfInt = {1,4,7,3,5};
+
+      String arrayOfString = Arrays.toString [arrayOfInt];
+
+      System.out.println ("arrayOfString");
+
+
+  }
